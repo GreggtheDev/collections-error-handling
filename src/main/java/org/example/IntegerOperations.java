9ff8bc9338a1b5c3c90d2b0653c2ce87957e4f9f
GreggtheDev <greggthedev@gmail.com>
2024-07-31 13:01:03 -0400

    added set to remove duplicates

--- src/main/java/org/example/IntegerOperations.java
+++ src/main/java/org/example/IntegerOperations.java
@@ -6,3 +6,6 @@ public class IntegerOperations {
     public static void main(String[] args) {
         // Define a List of integers with at least one set of duplicates
         List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 3, 2, 6, 7, 8, 9, 10, 5);
+
+        // Use a Set to remove duplicates
+        Set<Integer> integerSet = new HashSet<>(integerList);
