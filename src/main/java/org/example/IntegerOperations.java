c95913b6067b9e328bda7c63dbc4aab3bb63bb5d
GreggtheDev <greggthedev@gmail.com>
2024-07-31 12:58:34 -0400

    defined a list of integers with at least one duplicate

--- src/main/java/org/example/IntegerOperations.java
+++ src/main/java/org/example/IntegerOperations.java
@@ -1,4 +1,8 @@
 package org.example;
 
+import java.util.*;
+
 public class IntegerOperations {
-}
+    public static void main(String[] args) {
+        // Define a List of integers with at least one set of duplicates
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 3, 2, 6, 7, 8, 9, 10, 5);
